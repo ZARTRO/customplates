18781349bc37a31f9f02c44e23263c4a95a6875a
ZARTRO <87285902+ZARTRO@users.noreply.github.com>
2021-10-31 15:27:53 +0100

    Mutiple Plates
    Add more plates

--- CustomPlate/plate.lua
+++ CustomPlate/plate.lua
@@ -1,6 +1,92 @@
 --- IMAGE CONFIG HERE ---
-imageUrl = "Image URL Here" -- Paste your image URL here (doesn't have to be from imgur)
 
+-- Images
+imageUrl1 = "Image URL Here" -- Paste your image URL here (doesn't have to be from imgur)
+imageUrl2 = "Image URL Here" -- Paste your image URL here (doesn't have to be from imgur)
+imageUrl3 = "Image URL Here" -- Paste your image URL here (doesn't have to be from imgur)
+imageUrl4 = "Image URL Here" -- Paste your image URL here (doesn't have to be from imgur)
+imageUrl5 = "Image URL Here" -- Paste your image URL here (doesn't have to be from imgur)
+imageUrl6 = "Image URL Here" -- Paste your image URL here (doesn't have to be from imgur)
+
+-- Images N (Pink)
+imageUrl1n = "Image URL Here" -- Paste your image URL here (doesn't have to be from imgur)
+imageUrl2n = "Image URL Here" -- Paste your image URL here (doesn't have to be from imgur)
+imageUrl3n = "Image URL Here" -- Paste your image URL here (doesn't have to be from imgur)
+imageUrl4n = "Image URL Here" -- Paste your image URL here (doesn't have to be from imgur)
+imageUrl5n = "Image URL Here" -- Paste your image URL here (doesn't have to be from imgur)
+imageUrl6n = "Image URL Here" -- Paste your image URL here (doesn't have to be from imgur)
+
+-- ============================================================== PLATE 1 =====================================================================
+
+-- The actual script --
+local textureDic = CreateRuntimeTxd('duiTxd') -- Create custom texture dictionary only needs to be done once
+local object = CreateDui(imageUrl1, 540, 300) -- Load image into object
+local handle = GetDuiHandle(object) -- Gets DUI handle from object
+CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex", handle) -- Creates the texture "duiTex" in the "duiTxd" dictionary
+AddReplaceTexture('vehshare', 'plate01', 'duiTxd', 'duiTex') -- Applies "duiTex" from "duiTxd" to "plate01" from "vehshare"
+
+local object = CreateDui(imageUrl1n, 540, 300) -- this URL doesn't need to be edited, its just the 2d model for the plate -- Load image into object
+local handle = GetDuiHandle(object) -- Gets DUI handle from object
+CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex2", handle) -- Creates the texture "duiTex" in the "duiTxd" dictionary
+AddReplaceTexture('vehshare', 'plate01_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate01_n" from "vehshare"
+
+-- ============================================================== PLATE 2 =====================================================================
+
+-- The actual script --
+local textureDic = CreateRuntimeTxd('duiTxd') -- Create custom texture dictionary only needs to be done once
+local object = CreateDui(imageUrl2, 540, 300) -- Load image into object
+local handle = GetDuiHandle(object) -- Gets DUI handle from object
+CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex", handle) -- Creates the texture "duiTex" in the "duiTxd" dictionary
+AddReplaceTexture('vehshare', 'plate02', 'duiTxd', 'duiTex') -- Applies "duiTex" from "duiTxd" to "plate01" from "vehshare"
+
+local object = CreateDui(imageUrl2n, 540, 300) -- this URL doesn't need to be edited, its just the 2d model for the plate -- Load image into object
+local handle = GetDuiHandle(object) -- Gets DUI handle from object
+CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex2", handle) -- Creates the texture "duiTex" in the "duiTxd" dictionary
+AddReplaceTexture('vehshare', 'plate02_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate01_n" from "vehshare"
+
+-- ============================================================== PLATE 3 =====================================================================
+
+-- The actual script --
+local textureDic = CreateRuntimeTxd('duiTxd') -- Create custom texture dictionary only needs to be done once
+local object = CreateDui(imageUrl3, 540, 300) -- Load image into object
+local handle = GetDuiHandle(object) -- Gets DUI handle from object
+CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex", handle) -- Creates the texture "duiTex" in the "duiTxd" dictionary
+AddReplaceTexture('vehshare', 'plate03', 'duiTxd', 'duiTex') -- Applies "duiTex" from "duiTxd" to "plate01" from "vehshare"
+
+local object = CreateDui(imageUrl3n, 540, 300) -- this URL doesn't need to be edited, its just the 2d model for the plate -- Load image into object
+local handle = GetDuiHandle(object) -- Gets DUI handle from object
+CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex2", handle) -- Creates the texture "duiTex" in the "duiTxd" dictionary
+AddReplaceTexture('vehshare', 'plate03_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate01_n" from "vehshare"
+
+-- ============================================================== PLATE 4 =====================================================================
+
+-- The actual script --
+local textureDic = CreateRuntimeTxd('duiTxd') -- Create custom texture dictionary only needs to be done once
+local object = CreateDui(imageUrl4, 540, 300) -- Load image into object
+local handle = GetDuiHandle(object) -- Gets DUI handle from object
+CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex", handle) -- Creates the texture "duiTex" in the "duiTxd" dictionary
+AddReplaceTexture('vehshare', 'plate04', 'duiTxd', 'duiTex') -- Applies "duiTex" from "duiTxd" to "plate01" from "vehshare"
+
+local object = CreateDui(imageUrl4n, 540, 300) -- this URL doesn't need to be edited, its just the 2d model for the plate -- Load image into object
+local handle = GetDuiHandle(object) -- Gets DUI handle from object
+CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex2", handle) -- Creates the texture "duiTex" in the "duiTxd" dictionary
+AddReplaceTexture('vehshare', 'plate04_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate01_n" from "vehshare"
+
+-- ============================================================== PLATE 5 =====================================================================
+
+-- The actual script --
+local textureDic = CreateRuntimeTxd('duiTxd') -- Create custom texture dictionary only needs to be done once
+local object = CreateDui(imageUrl5, 540, 300) -- Load image into object
+local handle = GetDuiHandle(object) -- Gets DUI handle from object
+CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex", handle) -- Creates the texture "duiTex" in the "duiTxd" dictionary
+AddReplaceTexture('vehshare', 'plate05', 'duiTxd', 'duiTex') -- Applies "duiTex" from "duiTxd" to "plate01" from "vehshare"
+
+local object = CreateDui(imageUrl5n, 540, 300) -- this URL doesn't need to be edited, its just the 2d model for the plate -- Load image into object
+local handle = GetDuiHandle(object) -- Gets DUI handle from object
+CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex2", handle) -- Creates the texture "duiTex" in the "duiTxd" dictionary
+AddReplaceTexture('vehshare', 'plate05_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate01_n" from "vehshare"
+
+-- ============================================================== PLATE 6 =====================================================================
 
 -- The actual script --
 local textureDic = CreateRuntimeTxd('duiTxd') -- Create custom texture dictionary only needs to be done once
@@ -8,17 +94,8 @@ local object = CreateDui(imageUrl, 540, 300) -- Load image into object
 local handle = GetDuiHandle(object) -- Gets DUI handle from object
 CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex", handle) -- Creates the texture "duiTex" in the "duiTxd" dictionary
 AddReplaceTexture('vehshare', 'plate01', 'duiTxd', 'duiTex') -- Applies "duiTex" from "duiTxd" to "plate01" from "vehshare"
-AddReplaceTexture('vehshare', 'plate02', 'duiTxd', 'duiTex') -- Applies "duiTex" from "duiTxd" to "plate02" from "vehshare"
-AddReplaceTexture('vehshare', 'plate03', 'duiTxd', 'duiTex') -- Applies "duiTex" from "duiTxd" to "plate03" from "vehshare"
-AddReplaceTexture('vehshare', 'plate04', 'duiTxd', 'duiTex') -- Applies "duiTex" from "duiTxd" to "plate04" from "vehshare"
-AddReplaceTexture('vehshare', 'plate05', 'duiTxd', 'duiTex') -- Applies "duiTex" from "duiTxd" to "plate05" from "vehshare"
 
-
-local object = CreateDui('https://i.imgur.com/Q3uw6V7.png', 540, 300) -- this URL doesn't need to be edited, its just the 2d model for the plate -- Load image into object
+local object = CreateDui(imageUrl1n, 540, 300) -- this URL doesn't need to be edited, its just the 2d model for the plate -- Load image into object
 local handle = GetDuiHandle(object) -- Gets DUI handle from object
 CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex2", handle) -- Creates the texture "duiTex" in the "duiTxd" dictionary
 AddReplaceTexture('vehshare', 'plate01_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate01_n" from "vehshare"
-AddReplaceTexture('vehshare', 'plate02_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate02_n" from "vehshare"
-AddReplaceTexture('vehshare', 'plate03_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate03_n" from "vehshare"
-AddReplaceTexture('vehshare', 'plate04_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate04_n" from "vehshare"
-AddReplaceTexture('vehshare', 'plate05_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate05_n" from "vehshare"
